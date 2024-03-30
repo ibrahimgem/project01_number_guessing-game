02336f5a2208a0d29e4d15bed84e2ab72292411f
Muhammad Ibrahim <iby.khan7@gmail.com>
2024-03-30 23:52:14 +0500

    Number Guess Game

---
+++ main.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let randomNum: number = Math.floor(Math.random() * 10 + 1);
+let answers = await inquirer.prompt([
+    {
+        name: "userguessednum",
+        type: "number",
+        message: "Please guess a number between 1 to 10:"
+    },
+]);
+if (answers.userguessednum === randomNum) {
+    console.log("Congratulations! You guessed it right.");
+} else console.log("Ops, you guessed wrong number.");
